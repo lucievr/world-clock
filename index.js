3ac00b614e233064ca06baa0e716fff40cedd2e8
Lucie <46979603+lucievr@users.noreply.github.com>
2019-03-14 19:44:12 +0100

    Add location and current time display

--- index.js
+++ index.js
@@ -5,30 +5,65 @@ const minHand = document.querySelectorAll(".min-hand");
 const hourHand = document.querySelectorAll(".hour-hand");
 
 
-function setDate () {
+function setDateNY () {
   const currentTime = new Date();
-
- for(i=0; i<secondHand.length; i++) {
+  
   const seconds = currentTime.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90; 
-  secondHand[i].style.transform = `rotate(${secondsDegrees}deg)`;
+  secondHand[0].style.transform = `rotate(${secondsDegrees}deg)`;
 
-  for(j=0; j<minHand.length; j++) {
-    const mins = currentTime.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    minHand[j].style.transform = `rotate(${minsDegrees}deg)`;
+  const mins = currentTime.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minHand[0].style.transform = `rotate(${minsDegrees}deg)`;
 
-    for(k=0; k<hourHand.length; k++) {
-      const hours = currentTime.getHours();
-      const hoursDegrees = ((hours / 12) * 360) + ((mins/60)*30) + 90;
-      hourHand[k].style.transform = `rotate(${hoursDegrees}deg)`;
-    }
-  }
- }
+  const hours = (currentTime.getHours())-5;
+  const hoursDegrees = ((hours / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand[0].style.transform = `rotate(${hoursDegrees}deg)`;
+  document.querySelectorAll("h2.ny")[0].innerHTML = (`${hours} : ${mins} : ${seconds}`);
 }
 
-setInterval (setDate, 1000);
+setInterval (setDateNY, 1000);
+setDateNY();
 
-setDate();
+function setDatePrg () {
+  const currentTime = new Date();
+
+  const seconds = currentTime.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90; 
+  secondHand[1].style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = currentTime.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minHand[1].style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = currentTime.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand[1].style.transform = `rotate(${hoursDegrees}deg)`;
+  document.querySelectorAll("h2.prg")[0].innerHTML = (`${hours} : ${mins} : ${seconds}`);
+}
+
+setInterval (setDatePrg, 1000);
+setDatePrg();
+
+
+function setDateTok () {
+  const currentTime = new Date();
+
+  const seconds = currentTime.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90; 
+  secondHand[2].style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = currentTime.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minHand[2].style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = (currentTime.getHours()) + 8;
+  const hoursDegrees = ((hours / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand[2].style.transform = `rotate(${hoursDegrees}deg)`;
+  document.querySelectorAll("h2.tok")[0].innerHTML = (`${hours} : ${mins} : ${seconds}`);
+}
+
+setInterval (setDateTok, 1000);
+setDateTok();
 
 
